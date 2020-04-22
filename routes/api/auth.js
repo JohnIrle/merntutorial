f23bfa785b361d0663b1c043b6cd7bc8efd0c773
JohnIrle <irlejohn@gmail.com>
2020-04-22 15:51:05 -0500

    Initial commit

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,19 +8,18 @@ const auth = require('../../middleware/auth');
 
 const User = require('../../models/User');
 
-// @route   GET api/auth
-// @desc    Test route
-// @access  Public
+// @route    GET api/auth
+// @desc     Get user by token
+// @access   Private
 router.get('/', auth, async (req, res) => {
     try {
         const user = await User.findById(req.user.id).select('-password');
         res.json(user);
     } catch (err) {
         console.error(err.message);
-        res.status(500).send("Server Error");
+        res.status(500).send('Server Error');
     }
 });
-
 // @route   POST api/auth
 // @desc    Authenticate user and get token
 // @access  Public
